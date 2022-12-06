4e7e31ecd9f13c01a77c9df1941e9b9dd1fd5335
tom-osborne-xtrac <tom_osborne@xtrac.com>
2022-12-06 12:05:00 +0000

    Add field expressions

---
+++ Page 10/Inputs/INSTALLATION-onfield-changed.cs
@@ -0,0 +1,172 @@
+ /* ****** 1159 - On-leave Inputs.INSTALLATION.Value ****** */
+ 
+ if (Inputs.INSTALLATION.Value == "Audi") {
+ Inputs.DRAWING_VARIANT.Value = "1359-900-000A";
+ Inputs.FIRST_RATIO.Value = "13:35";
+ Inputs.SECOND_RATIO.Value = "16:33";
+ Inputs.THIRD_RATIO.Value = "16:27";
+ Inputs.FOURTH_RATIO.Value = "19:27";
+ Inputs.FIFTH_RATIO.Value = "19:23";
+ Inputs.SIXTH_RATIO.Value = "19:20";
+ Inputs.SEVENTH_RATIO.Value = "27:25";
+ Inputs.BEVEL_RATIO.Value = "21:22";
+ Inputs.FINAL_RATIO.Value = "15:47";
+ Inputs.DRIVE.Value = "45";
+ Inputs.COAST.Value = "60";
+ Inputs.OUTFLANGE_REQD.Value = "plug kit";
+ Inputs.HYBRID.Value = "19/30/44";
+ Inputs.AC_PULLEY.Value = "Yes";
+ Inputs.PACKING_CRATE.Value = "Yes";
+ Inputs.MANIFOLD.Value = "standard";
+ Inputs.INCLUDE1254GEARCHANGE.Value = "n/a";
+ Inputs.INCLUDEBELLHOUSINGDOWELS.Value = "Yes";
+ }
+ 
+ if (Inputs.INSTALLATION.Value == "BMW") {
+ Inputs.DRAWING_VARIANT.Value = "1359-900-000A";
+ Inputs.FIRST_RATIO.Value = "13:35";
+ Inputs.SECOND_RATIO.Value = "15:33";
+ Inputs.THIRD_RATIO.Value = "18:31";
+ Inputs.FOURTH_RATIO.Value = "19:27";
+ Inputs.FIFTH_RATIO.Value = "19:23";
+ Inputs.SIXTH_RATIO.Value = "19:20";
+ Inputs.SEVENTH_RATIO.Value = "27:25";
+ Inputs.BEVEL_RATIO.Value = "22:21";
+ Inputs.FINAL_RATIO.Value = "13:49";
+ Inputs.DRIVE.Value = "30";
+ Inputs.COAST.Value = "60";
+ Inputs.OUTFLANGE_REQD.Value = "plug kit";
+ Inputs.HYBRID.Value = "19/30/44";
+ Inputs.AC_PULLEY.Value = "No";
+ Inputs.MANIFOLD.Value = "standard";
+ Inputs.INCLUDE1254GEARCHANGE.Value = "n/a";
+ Inputs.INCLUDEBELLHOUSINGDOWELS.Value = "Yes";
+ }
+
+ if (Inputs.INSTALLATION.Value == "BMW - Test") {
+ Inputs.DRAWING_VARIANT.Value = "1359-900-000A";
+ Inputs.FIRST_RATIO.Value = "12:37";
+ Inputs.SECOND_RATIO.Value = "14:33";
+ Inputs.THIRD_RATIO.Value = "16:31";
+ Inputs.FOURTH_RATIO.Value = "16:26";
+ Inputs.FIFTH_RATIO.Value = "18:25";
+ Inputs.SIXTH_RATIO.Value = "21:26";
+ Inputs.SEVENTH_RATIO.Value = "23:26";
+ Inputs.BEVEL_RATIO.Value = "22:21";
+ Inputs.FINAL_RATIO.Value = "15:46";
+ Inputs.DRIVE.Value = "30";
+ Inputs.COAST.Value = "60";
+ Inputs.OUTFLANGE_REQD.Value = "plug kit";
+ Inputs.HYBRID.Value = "18/31/43";
+ Inputs.AC_PULLEY.Value = "Yes";
+ Inputs.MANIFOLD.Value = "standard";
+ Inputs.INCLUDE1254GEARCHANGE.Value = "n/a";
+ Inputs.INCLUDEBELLHOUSINGDOWELS.Value = "Yes";
+ }
+ 
+ if (Inputs.INSTALLATION.Value == "Dallara") {
+ Inputs.DRAWING_VARIANT.Value = "1359-900-000A";
+ Inputs.FIRST_RATIO.Value = "13:35";
+ Inputs.SECOND_RATIO.Value = "16:33";
+ Inputs.THIRD_RATIO.Value = "16:27";
+ Inputs.FOURTH_RATIO.Value = "19:27";
+ Inputs.FIFTH_RATIO.Value = "21:26";
+ Inputs.SIXTH_RATIO.Value = "23:25";
+ Inputs.SEVENTH_RATIO.Value = "24:23";
+ Inputs.BEVEL_RATIO.Value = "23:21";
+ Inputs.FINAL_RATIO.Value = "13:49";
+ Inputs.OUTFLANGE_REQD.Value = "";
+ Inputs.PRELOAD_VALUE.Value = "0";
+ Inputs.AC_PULLEY.Value = "Yes";
+ Inputs.MANIFOLD.Value = "standard";
+ Inputs.INCLUDE1254GEARCHANGE.Value = "n/a";
+ Inputs.INCLUDEBELLHOUSINGDOWELS.Value = "Yes";
+ }
+ 
+ if (Inputs.INSTALLATION.Value == "GM") {
+ Inputs.DRAWING_VARIANT.Value = "1359-900-000A";
+ Inputs.FIRST_RATIO.Value = "13:35";
+ Inputs.SECOND_RATIO.Value = "16:33";
+ Inputs.THIRD_RATIO.Value = "16:27";
+ Inputs.FOURTH_RATIO.Value = "19:27";
+ Inputs.FIFTH_RATIO.Value = "21:26";
+ Inputs.SIXTH_RATIO.Value = "23:25";
+ Inputs.SEVENTH_RATIO.Value = "24:23";
+ Inputs.BEVEL_RATIO.Value = "23:21";
+ Inputs.FINAL_RATIO.Value = "13:49";
+ Inputs.DRIVE.Value = "30";
+ Inputs.COAST.Value = "60";
+ Inputs.OUTFLANGE_REQD.Value = "plug kit";
+ Inputs.PRELOAD_VALUE.Value = "0";
+ Inputs.HYBRID.Value = "21/28/46";
+ Inputs.AC_PULLEY.Value = "Yes";
+ Inputs.MANIFOLD.Value = "standard";
+ Inputs.INCLUDE1254GEARCHANGE.Value = "n/a";
+ Inputs.INCLUDEBELLHOUSINGDOWELS.Value = "Yes";
+ }
+ 
+ if (Inputs.INSTALLATION.Value == "HPD - Short") {
+ Inputs.DRAWING_VARIANT.Value = "1359-900-000A";
+ Inputs.FIRST_RATIO.Value = "14:34";
+ Inputs.SECOND_RATIO.Value = "16:32";
+ Inputs.THIRD_RATIO.Value = "18:31";
+ Inputs.FOURTH_RATIO.Value = "17:26";
+ Inputs.FIFTH_RATIO.Value = "20:27";
+ Inputs.SIXTH_RATIO.Value = "19:23";
+ Inputs.SEVENTH_RATIO.Value = "19:20";
+ Inputs.BEVEL_RATIO.Value = "21:23";
+ Inputs.FINAL_RATIO.Value = "13:46";
+ Inputs.DRIVE.Value = "30";
+ Inputs.COAST.Value = "60";
+ Inputs.OUTFLANGE_REQD.Value = "driveshaft plunger kit";
+ Inputs.HYBRID.Value = "25/24/50";
+ Inputs.AC_PULLEY.Value = "No";
+ Inputs.MANIFOLD.Value = "standard";
+ Inputs.INCLUDE1254GEARCHANGE.Value = "n/a";
+ Inputs.INCLUDEBELLHOUSINGDOWELS.Value = "No";
+ }
+ 
+ if (Inputs.INSTALLATION.Value == "Porsche") {
+ Inputs.DRAWING_VARIANT.Value = "1359-900-000A";
+ Inputs.FIRST_RATIO.Value = "13:35";
+ Inputs.SECOND_RATIO.Value = "16:33";
+ Inputs.THIRD_RATIO.Value = "16:27";
+ Inputs.FOURTH_RATIO.Value = "19:27";
+ Inputs.FIFTH_RATIO.Value = "19:23";
+ Inputs.SIXTH_RATIO.Value = "19:20";
+ Inputs.SEVENTH_RATIO.Value = "27:25";
+ Inputs.BEVEL_RATIO.Value = "21:22";
+ Inputs.FINAL_RATIO.Value = "14:48";
+ Inputs.DRIVE.Value = "45";
+ Inputs.COAST.Value = "70";
+ Inputs.OUTFLANGE_REQD.Value = "plug kit";
+ Inputs.PRELOAD_VALUE.Value = "50";
+ Inputs.HYBRID.Value = "19/30/44";
+ Inputs.AC_PULLEY.Value = "Yes";
+ Inputs.PACKING_CRATE.Value = "Yes";
+ Inputs.INCLUDE1254GEARCHANGE.Value = "Yes";
+ Inputs.MANIFOLD.Value = "customer specific";
+ Inputs.INCLUDEBELLHOUSINGDOWELS.Value = "No";
+ }
+
+if (Inputs.INSTALLATION.Value == "HPD - Daytona")
+{
+Inputs.DRAWING_VARIANT.Value = "1359-900-000A";
+Inputs.FIRST_RATIO.Value = "14:34";
+ Inputs.SECOND_RATIO.Value = "16:33";
+ Inputs.THIRD_RATIO.Value = "17:30";
+ Inputs.FOURTH_RATIO.Value = "17:26";
+ Inputs.FIFTH_RATIO.Value = "20:27";
+ Inputs.SIXTH_RATIO.Value = "19:23";
+ Inputs.SEVENTH_RATIO.Value = "23:25";
+ Inputs.BEVEL_RATIO.Value = "21:23";
+ Inputs.FINAL_RATIO.Value = "14:48";
+ Inputs.DRIVE.Value = "30";
+ Inputs.COAST.Value = "60";
+ Inputs.OUTFLANGE_REQD.Value = "driveshaft plunger kit";
+ Inputs.HYBRID.Value = "25/24/50";
+ Inputs.AC_PULLEY.Value = "No";
+ Inputs.MANIFOLD.Value = "standard";
+ Inputs.INCLUDE1254GEARCHANGE.Value = "n/a";
+ Inputs.INCLUDEBELLHOUSINGDOWELS.Value = "No";
+}
---
+++ Page 30/PRELOAD_VALUE-on-field-changed.cs
@@ -0,0 +1,29 @@
+ /* ****** 1159 - On-leave Inputs.PRELOAD_VALUE.Value ****** */
+ 
+ if (Inputs.PRELOAD_VALUE.Value == "50") {
+ 	Inputs.PRELOAD_SHIM.Value = "233-290-103Z";
+ }
+ 
+ if (Inputs.PRELOAD_VALUE.Value == "65") {
+ 	Inputs.PRELOAD_SHIM.Value = "233-290-103A";
+ }
+ 
+ if (Inputs.PRELOAD_VALUE.Value == "85") {
+ 	Inputs.PRELOAD_SHIM.Value = "233-290-103B";
+ }
+ 
+ if (Inputs.PRELOAD_VALUE.Value == "110") {
+ 	Inputs.PRELOAD_SHIM.Value = "233-290-103C";
+ }
+ 
+ if (Inputs.PRELOAD_VALUE.Value == "140") {
+ 	Inputs.PRELOAD_SHIM.Value = "233-290-103D";
+ }
+ 
+ if (Inputs.PRELOAD_VALUE.Value == "175") {
+ 	Inputs.PRELOAD_SHIM.Value = "233-290-103E";
+ }
+ 
+ if (Inputs.PRELOAD_VALUE.Value == "200") {
+ 	Inputs.PRELOAD_SHIM.Value = "233-290-103A";
+ }

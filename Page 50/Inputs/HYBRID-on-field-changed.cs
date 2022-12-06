782c5a2a9c8b72177385c6d02f690cab246d5d1c
tom-osborne-xtrac <tom_osborne@xtrac.com>
2022-12-06 12:08:00 +0000

    refactor: whitespace only

--- Page 50/Inputs/HYBRID-on-field-changed.cs
+++ Page 50/Inputs/HYBRID-on-field-changed.cs
@@ -1,61 +1,61 @@
- /* ****** 1359 - On-leave Inputs.HYBRID.Value ****** */
- 
- if (Inputs.HYBRID.Value == "16/33/41") {
- 	Inputs.Z1_PART.Value = "1359-441-1A-1633";
- 	Inputs.Z2_PART.Value = "1359-441-2A-1633";
- 	Inputs.Z3_PART.Value = "1359-441-0A-3341";
- }
- 
- if (Inputs.HYBRID.Value == "17/32/42") {
- 	Inputs.Z1_PART.Value = "1359-441-1A-1732";
- 	Inputs.Z2_PART.Value = "1359-441-2A-1732";
- 	Inputs.Z3_PART.Value = "1359-441-0A-3242";
- }
- 
- if (Inputs.HYBRID.Value == "18/31/43") {
- 	Inputs.Z1_PART.Value = "1359-441-1A-1831";
- 	Inputs.Z2_PART.Value = "1359-441-2A-1831";
- 	Inputs.Z3_PART.Value = "1359-441-0A-3143";
- }
- 
- if (Inputs.HYBRID.Value == "19/30/44") {
- 	Inputs.Z1_PART.Value = "1359-441-1A-1930";
- 	Inputs.Z2_PART.Value = "1359-441-2A-1930";
- 	Inputs.Z3_PART.Value = "1359-441-0A-3044";
- }
- 
- if (Inputs.HYBRID.Value == "20/29/45") {
- 	Inputs.Z1_PART.Value = "1359-441-1A-2029";
- 	Inputs.Z2_PART.Value = "1359-441-2A-2029";
- 	Inputs.Z3_PART.Value = "1359-441-0A-2945";
- }
- 
- if (Inputs.HYBRID.Value == "21/28/46") {
- 	Inputs.Z1_PART.Value = "1359-441-1A-2128";
- 	Inputs.Z2_PART.Value = "1359-441-2A-2128";
- 	Inputs.Z3_PART.Value = "1359-441-0A-2846";
- }
- 
- if (Inputs.HYBRID.Value == "22/27/47") {
- 	Inputs.Z1_PART.Value = "1359-441-1A-2227";
- 	Inputs.Z2_PART.Value = "1359-441-2A-2227";
- 	Inputs.Z3_PART.Value = "1359-441-0A-2747";
- }
- 
- if (Inputs.HYBRID.Value == "23/26/48") {
- 	Inputs.Z1_PART.Value = "1359-441-1A-2326";
- 	Inputs.Z2_PART.Value = "1359-441-2A-2326";
- 	Inputs.Z3_PART.Value = "1359-441-0A-2648";
- }
- 
- if (Inputs.HYBRID.Value == "24/25/49") {
- 	Inputs.Z1_PART.Value = "1359-441-1A-2425";
- 	Inputs.Z2_PART.Value = "1359-441-2A-2425";
- 	Inputs.Z3_PART.Value = "1359-441-0A-2549";
- }
- 
- if (Inputs.HYBRID.Value == "25/24/50") {
- 	Inputs.Z1_PART.Value = "1359-441-1A-2524";
- 	Inputs.Z2_PART.Value = "1359-441-2A-2524";
- 	Inputs.Z3_PART.Value = "1359-441-0A-2450";
- }
+/* ****** 1359 - On-leave Inputs.HYBRID.Value ****** */
+
+if (Inputs.HYBRID.Value == "16/33/41") {
+    Inputs.Z1_PART.Value = "1359-441-1A-1633";
+    Inputs.Z2_PART.Value = "1359-441-2A-1633";
+    Inputs.Z3_PART.Value = "1359-441-0A-3341";
+}
+
+if (Inputs.HYBRID.Value == "17/32/42") {
+    Inputs.Z1_PART.Value = "1359-441-1A-1732";
+    Inputs.Z2_PART.Value = "1359-441-2A-1732";
+    Inputs.Z3_PART.Value = "1359-441-0A-3242";
+}
+
+if (Inputs.HYBRID.Value == "18/31/43") {
+    Inputs.Z1_PART.Value = "1359-441-1A-1831";
+    Inputs.Z2_PART.Value = "1359-441-2A-1831";
+    Inputs.Z3_PART.Value = "1359-441-0A-3143";
+}
+
+if (Inputs.HYBRID.Value == "19/30/44") {
+    Inputs.Z1_PART.Value = "1359-441-1A-1930";
+    Inputs.Z2_PART.Value = "1359-441-2A-1930";
+    Inputs.Z3_PART.Value = "1359-441-0A-3044";
+}
+
+if (Inputs.HYBRID.Value == "20/29/45") {
+    Inputs.Z1_PART.Value = "1359-441-1A-2029";
+    Inputs.Z2_PART.Value = "1359-441-2A-2029";
+    Inputs.Z3_PART.Value = "1359-441-0A-2945";
+}
+
+if (Inputs.HYBRID.Value == "21/28/46") {
+    Inputs.Z1_PART.Value = "1359-441-1A-2128";
+    Inputs.Z2_PART.Value = "1359-441-2A-2128";
+    Inputs.Z3_PART.Value = "1359-441-0A-2846";
+}
+
+if (Inputs.HYBRID.Value == "22/27/47") {
+    Inputs.Z1_PART.Value = "1359-441-1A-2227";
+    Inputs.Z2_PART.Value = "1359-441-2A-2227";
+    Inputs.Z3_PART.Value = "1359-441-0A-2747";
+}
+
+if (Inputs.HYBRID.Value == "23/26/48") {
+    Inputs.Z1_PART.Value = "1359-441-1A-2326";
+    Inputs.Z2_PART.Value = "1359-441-2A-2326";
+    Inputs.Z3_PART.Value = "1359-441-0A-2648";
+}
+
+if (Inputs.HYBRID.Value == "24/25/49") {
+    Inputs.Z1_PART.Value = "1359-441-1A-2425";
+    Inputs.Z2_PART.Value = "1359-441-2A-2425";
+    Inputs.Z3_PART.Value = "1359-441-0A-2549";
+}
+
+if (Inputs.HYBRID.Value == "25/24/50") {
+    Inputs.Z1_PART.Value = "1359-441-1A-2524";
+    Inputs.Z2_PART.Value = "1359-441-2A-2524";
+    Inputs.Z3_PART.Value = "1359-441-0A-2450";
+}
